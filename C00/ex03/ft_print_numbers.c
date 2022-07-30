a1207c1e768cf6366519e644ed6aa6d470065b86
JasonMontenegro <110299453+JasonMontenegro@users.noreply.github.com>
2022-07-30 17:03:32 +0100

    Add files via upload

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ogziri <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 00:46:59 by ogziri            #+#    #+#             */
+/*   Updated: 2022/07/26 08:52:43 by ogziri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+		number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
